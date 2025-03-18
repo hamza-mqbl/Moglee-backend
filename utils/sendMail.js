41e084e205237ba5bf4ee7663f6625a2ba4052a9
hamza-mqbl <hamzaaliics@gmail.com>
2025-03-18 21:31:06 +0500

    mail

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,23 +1,32 @@
 const nodemailer = require("nodemailer");
+
 const sendMail = async (options) => {
-  const transport = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    service: process.env.SMTP_SERVICE,
-    auth: {
-      user: process.env.SMTP_MAIL,
-      pass: process.env.SMTP_PASSWORD,
-    },
-  });
+  try {
+    const transport = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      service: process.env.SMTP_SERVICE,
+      auth: {
+        user: process.env.SMTP_MAIL,
+        pass: process.env.SMTP_PASSWORD,
+      },
+    });
 
-  const mailOptions = {
-    from: process.env.SMTP_MAIL,
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-  };
+    const mailOptions = {
+      from: process.env.SMTP_MAIL,
+      to: options.email,
+      subject: options.subject,
+      text: options.message,
+    };
 
-  await transport.sendMail(mailOptions);
+    console.log("🚀 ~ sendMail ~ mailOptions:", mailOptions);
+    
+    await transport.sendMail(mailOptions);
+    console.log("✅ Email sent successfully");
+  } catch (error) {
+    console.error("❌ Error sending email:", error);
+    throw new Error("Email sending failed");
+  }
 };
 
 module.exports = sendMail;
