018c901d4f66ef14393b5cd718463f987e5f3153
hamza-mqbl <hamzaaliics@gmail.com>
2025-03-18 21:04:39 +0500

    production issue fix

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -1,19 +1,22 @@
+const jwt = require("jsonwebtoken");
 
-// create token and saving in cookies
-const jwt=require("jsonwebtoken")
 const sendToken = (userId, statuscode, res) => {
   const token = jwt.sign({ id: userId }, process.env.JWT_SECRET_KEY, {
     expiresIn: "5h",
   });
 
-  // option for cookies
+  // Options for cookies
   const options = {
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
     httpOnly: true,
+    secure: process.env.NODE_ENV === "production", // Required for HTTPS
+    sameSite: "None", // Important for cross-origin cookies
   };
+
   res.status(statuscode).cookie("token", token, options).json({
     success: true,
     token,
   });
 };
-module.exports=sendToken;
+
+module.exports = sendToken;
